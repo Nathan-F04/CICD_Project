306f47402eb6819d9dd13eeb82bc48dfec3279fb
Shine Sujith <g00414454@atu.ie>
2024-12-16 12:22:56 +0000

    removed unused imports

--- src/main/java/ie/atu/cicd_project/StocksRepository.java
+++ src/main/java/ie/atu/cicd_project/StocksRepository.java
@@ -2,9 +2,6 @@ package ie.atu.cicd_project;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.Optional;
 
 @Repository
 public interface StocksRepository extends JpaRepository<Stocks, Long> {
