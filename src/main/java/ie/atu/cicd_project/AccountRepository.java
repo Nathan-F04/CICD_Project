35ecc1a60566a4b782bb4ea73e85af2b38228134
Shine Sujith <g00414454@atu.ie>
2024-12-18 16:35:16 +0000

    tested the display account method

--- src/main/java/ie/atu/cicd_project/AccountRepository.java
+++ src/main/java/ie/atu/cicd_project/AccountRepository.java
@@ -7,5 +7,5 @@ import java.util.Optional;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
-    Optional<Account> findByName(String name);
+    Account findByName(String name);
 }
