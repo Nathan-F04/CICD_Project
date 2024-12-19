907680f9db86899f9fed4d423051c9a8144d4d41
Shine Sujith <g00414454@atu.ie>
2024-12-19 19:56:03 +0000

    Added three other Repos

---
+++ StockValue-Service/src/test/java/ie/atu/stockvalueservice/StockValueServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.stockvalueservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockValueServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
