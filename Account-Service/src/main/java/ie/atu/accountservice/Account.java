c18932afa9c2c0059dfc55b761d96b736b80a54b
Shine Sujith <g00414454@atu.ie>
2024-12-19 20:21:45 +0000

    added Account service

---
+++ Account-Service/src/main/java/ie/atu/accountservice/Account.java
@@ -0,0 +1,30 @@
+package ie.atu.accountservice;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long personId;
+    @NotEmpty(message = "Name can not be empty")
+    private String name;
+    @Min(0)
+    private float bankBal;
+
+    public Account(String name, float bankBal) {
+        this.name = name;
+        this.bankBal = bankBal;
+    }
+}
