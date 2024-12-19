aab306a3c485b1937dcf76fef2c2bb61b86f4bc9
Shine Sujith <g00414454@atu.ie>
2024-12-19 20:39:46 +0000

    added Person-service

---
+++ Person-Service/src/main/java/ie/atu/personservice/Person.java
@@ -0,0 +1,39 @@
+package ie.atu.personservice;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //@NotNull(message = "Id must not be blank")
+    private Long personId;
+    @NotEmpty(message = "Name must not be blank")
+    private String name;
+    @Email(message = "Email must be in valid format")
+    private String email;
+    @NotEmpty(message = "Password must not be blank")
+    private String password;
+    @NotEmpty(message = "Bank details must not be blank")
+    private String bankDetails;
+    @PositiveOrZero(message = "Stock portfolio must be positive")
+    private String stock_portfolio;
+
+    //Constructor for data loader
+    public Person(String name, String email, String password, String bankDetails, String stock_portfolio) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.bankDetails = bankDetails;
+        this.stock_portfolio = stock_portfolio;
+    }
+}
---
+++ Person-Service/src/main/java/ie/atu/personservice/PersonService.java
@@ -0,0 +1,109 @@
+package ie.atu.personservice;
+
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+
+@Service
+public class PersonService {
+
+    private final PersonRepository personRepository;
+    private final PersonClient personClient;
+
+    public PersonService(PersonRepository personRepository, PersonClient personClient) {
+        this.personRepository = personRepository;
+        this.personClient = personClient;
+    }
+
+    //Actually get the requests done here
+    //sign up
+    public void signUpPerson(Person person) {
+        //save person to db to sign them up
+        personRepository.save(person);
+    }
+    //sign in
+    public void signInPerson(String name, String password) {
+        Optional<Person> verifyPerson = personRepository.findByName(name);
+        if(verifyPerson.isPresent()){
+            Person existingPerson = verifyPerson.get();
+            String verifyPassword = existingPerson.getPassword();
+            if (verifyPassword.equals(password)) {
+                System.out.println("Passwords match");
+            }else {
+                //change to actual error handling
+                System.out.println("Passwords do not match");
+            }
+        }else{
+            System.out.println("Person does not exist");
+        }
+    }
+
+    //View profile
+    public Person viewPersonProfile(String name, String password) {
+
+        Optional<Person> verifyPerson = personRepository.findByName(name);
+        if(verifyPerson.isPresent()){
+            Person existingPerson = verifyPerson.get();
+            String verifyPassword = existingPerson.getPassword();
+            if (verifyPassword.equals(password)) {
+                System.out.println("Passwords match");
+                return existingPerson;
+            }else {
+                //change to actual error handling
+                return null;
+            }
+        }else{
+            return null;
+        }
+    }
+
+    //edit profile
+    public void editPersonProfile(String name, String password, Person personEdit) {
+        Optional<Person> verifyPerson = personRepository.findByName(name);
+
+        if(verifyPerson.isPresent()){
+            Person existingPerson = verifyPerson.get();
+            String verifyPassword = existingPerson.getPassword();
+            if (verifyPassword.equals(password)) {
+                System.out.println("Passwords match");
+                existingPerson.setName(personEdit.getName());
+                existingPerson.setEmail(personEdit.getEmail());
+                existingPerson.setPassword(personEdit.getPassword());
+                existingPerson.setStock_portfolio(personEdit.getStock_portfolio());
+                existingPerson.setBankDetails(personEdit.getBankDetails());
+                System.out.println("Person edited successfully");
+
+            }else {
+                //change to actual error handling
+                System.out.println("Error editing person");
+            }
+        }else{
+            System.out.println("Error editing person, person may not exist");
+        }
+    }
+
+    //delete
+    public void DeleteAccount(Person person) {
+        //delete person from db
+        personRepository.delete(person);
+    }
+
+    //take email and password and if correct, call other func passing the name
+    public void myPortfolioValue(String name, String password) {
+        Optional<Person> verifyPerson = personRepository.findByName(name);
+
+        if(verifyPerson.isPresent()){
+            Person existingPerson = verifyPerson.get();
+            String verifyPassword = existingPerson.getPassword();
+            if (verifyPassword.equals(password)) {
+                System.out.println("Passwords match");
+                personClient.stockFindVal(name);
+
+            }else {
+                //change to actual error handling
+                System.out.println("Error editing person");
+            }
+        }else{
+            System.out.println("Error editing person, person may not exist");
+        }
+    }
+}
