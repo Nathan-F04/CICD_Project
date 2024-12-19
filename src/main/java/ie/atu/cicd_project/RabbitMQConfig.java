0cd810b0dc509da29d2b528d6eee2f74020765f9
Nathan <G00413711@atu.ie>
2024-12-19 19:04:30 +0000

    comments for understanding

--- src/main/java/ie/atu/cicd_project/RabbitMQConfig.java
+++ src/main/java/ie/atu/cicd_project/RabbitMQConfig.java
@@ -10,11 +10,14 @@ import org.springframework.amqp.core.Queue;
 @Configuration
 public class RabbitMQConfig {
 
+    //queues constants
     public static final String ORDERS_QUEUE = "ordersQueue";
     public static final String INVENTORY_UPDATE_QUEUE = "inventoryUpdateQueue";
     public static final String PAYMENT_PROCESSED_QUEUE = "paymentProcessedQueue";
     public static final String EXCHANGE = "orderExchange";
-    @Bean
+
+    //declare queues
+   @Bean
    public Queue ordersQueue(){
        return new Queue(ORDERS_QUEUE, true);
    }
@@ -28,11 +31,13 @@ public class RabbitMQConfig {
     public Queue paymentProcessedQueue(){
         return new Queue(PAYMENT_PROCESSED_QUEUE, true);
     }
+    //exchange here
     @Bean
-    public Queue orderExchange(){
-        return new Queue(EXCHANGE, true);
+    public TopicExchange exchange(){
+        return new TopicExchange(EXCHANGE);
     }
 
+    //binding for binding keys ie this case anything starting with order for ordersQueue
     @Bean
     public Binding ordersQueueBinding(Queue ordersQueue, TopicExchange exchange){
         return BindingBuilder.bind(ordersQueue).to(exchange).with("order.#");
