8215c79b855cb8abe697d6fbc5e79fbf8276c1cc
Nathan <G00413711@atu.ie>
2024-12-21 14:00:03 +0000

    new routing key, message converter and template

--- Person-Service/src/main/java/ie/atu/personservice/RabbitMQConfig.java
+++ Person-Service/src/main/java/ie/atu/personservice/RabbitMQConfig.java
@@ -1,9 +1,10 @@
 package ie.atu.personservice;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,36 +12,50 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitMQConfig {
 
     //queues constants
-    public static final String ORDERS_QUEUE = "ordersQueue";
+    public static final String PERSON_QUEUE = "personQueue";
     public static final String INVENTORY_UPDATE_QUEUE = "inventoryUpdateQueue";
     public static final String PAYMENT_PROCESSED_QUEUE = "paymentProcessedQueue";
     public static final String EXCHANGE = "orderExchange";
+    public static final String ROUTING_KEY = "person_routingKey";
 
     //declare queues
-   @Bean
-   public Queue ordersQueue(){
-       return new Queue(ORDERS_QUEUE, true);
-   }
+    @Bean
+    public Queue personQueue(){
+       return new Queue(PERSON_QUEUE, true);
+    }
 
-   @Bean
-   public Queue inventoryUpdateQueue(){
+    @Bean
+    public Queue inventoryUpdateQueue(){
         return new Queue(INVENTORY_UPDATE_QUEUE, true);
     }
 
-   @Bean
-   public Queue paymentProcessedQueue(){
+    @Bean
+    public Queue paymentProcessedQueue(){
         return new Queue(PAYMENT_PROCESSED_QUEUE, true);
     }
-   //exchange here
-   @Bean
-   public TopicExchange exchange(){
+    //exchange here
+    @Bean
+    public TopicExchange exchange(){
         return new TopicExchange(EXCHANGE);
     }
 
-   //binding for binding keys ie this case anything starting with order for ordersQueue
+    //binding for binding keys ie this case anything starting with order for ordersQueue
+    @Bean
+    public Binding ordersQueueBinding(Queue personQueue, TopicExchange exchange){
+        return BindingBuilder.bind(personQueue).to(exchange).with(ROUTING_KEY);
+   }
+
+   //message converter
+    @Bean
+    public MessageConverter messageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
+
    @Bean
-   public Binding ordersQueueBinding(Queue ordersQueue, TopicExchange exchange){
-        return BindingBuilder.bind(ordersQueue).to(exchange).with("order.#");
+   public AmqpTemplate template(ConnectionFactory connectionFactory){
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter());
+        return template;
    }
 }
 
