f6bbc8bb0bce074aba6ed1a18e61911cdd121c87
Nathan <G00413711@atu.ie>
2024-12-22 12:34:23 +0000

    created new stocks and made new person named bill and loaded in person table and stocks

--- Person-Service/src/main/java/ie/atu/personservice/PersonDataLoader.java
+++ Person-Service/src/main/java/ie/atu/personservice/PersonDataLoader.java
@@ -16,11 +16,13 @@ public class PersonDataLoader implements CommandLineRunner {
         //Insert data here
         Person testData1 = new Person("Paul" , "paul@atu.ie","pass1", "AIB", "1");
         Person testData2 = new Person("Nathan" , "nathan@atu.ie","pass2", "AIB", "2");
-        Person testData3 = new Person("Shine", "shine@atu.ie","pass3", "AIB", "3");
+        Person testData3 = new Person("Shine", "shine@atu.ie","pass3", "BOI", "3");
+        Person testData4 = new Person("Bill", "bill@atu.ie","pass4", "BOI", "4");
         //save here
         personRepository.save(testData1);
         personRepository.save(testData2);
         personRepository.save(testData3);
+        personRepository.save(testData4);
         //delete here
         personRepository.delete(testData1);
     }
