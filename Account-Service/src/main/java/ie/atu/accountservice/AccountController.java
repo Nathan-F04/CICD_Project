792a06b301295171d67a7a24770dd371e229c11d
Nathan <G00413711@atu.ie>
2024-12-27 14:22:40 +0000

    annotation changes

--- Account-Service/src/main/java/ie/atu/accountservice/AccountController.java
+++ Account-Service/src/main/java/ie/atu/accountservice/AccountController.java
@@ -1,5 +1,6 @@
 package ie.atu.accountservice;
 
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
--- Person-Service/src/main/java/ie/atu/personservice/PersonController.java
+++ Person-Service/src/main/java/ie/atu/personservice/PersonController.java
@@ -31,24 +31,24 @@ public class PersonController {
     }
     //sign in
     @GetMapping("/signIn/{name}/{password}")
-    public ResponseEntity<?>signIn(@Valid @PathVariable String name, @Valid @PathVariable String password) {
+    public ResponseEntity<?>signIn(@PathVariable String name, @PathVariable String password) {
         personService.signInPerson(name, password);
         return new ResponseEntity<>("Signed in successfully", HttpStatus.OK);
     }
     //get request to check details
     @GetMapping("/viewProfile/{name}/{password}")
-    public ResponseEntity<?>viewProfile(@Valid @PathVariable String name, @Valid @PathVariable String password) {
+    public ResponseEntity<?>viewProfile(@PathVariable String name, @PathVariable String password) {
         Person person = personService.viewPersonProfile(name, password);
         return ResponseEntity.ok(person);
     }
     //Delete account
     @DeleteMapping("/removeAccount/{name}")
-    public void RemoveAccount(@Valid @PathVariable String name){
+    public void RemoveAccount(@PathVariable String name){
         personService.DeleteAccount(name);
     }
     //update user details
     @PutMapping("/editProfile/{name}/{password}")
-    public ResponseEntity<?>EditProfile(@Valid @PathVariable String name, @Valid @PathVariable String password, @Valid @RequestBody Person personEdit) {
+    public ResponseEntity<?>EditProfile( @PathVariable String name,  @PathVariable String password,  @RequestBody Person personEdit) {
         personService.editPersonProfile(name, password, personEdit);
         return new ResponseEntity<>("Edited account successfully", HttpStatus.OK);
     }
@@ -56,7 +56,7 @@ public class PersonController {
     //enter a password and name
     //cal func-openfeign+mqtt
     @GetMapping("/PortfolioValue/{name}/{password}")
-    public ResponseEntity<?> portfolioValue(@Valid @PathVariable String name, @Valid @PathVariable String password){
+    public ResponseEntity<?> portfolioValue( @PathVariable String name, @PathVariable String password){
         Map<String, String> userDetails = new HashMap<>();
         userDetails.put("name", name);
         userDetails.put("password", password);
