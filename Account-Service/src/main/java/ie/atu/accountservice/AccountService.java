2e6bfd1148dae9126e783869afc4be84307e0037
Shine Sujith <g00414454@atu.ie>
2024-12-28 18:08:46 +0000

    made it easier to delete an account

--- Account-Service/src/main/java/ie/atu/accountservice/AccountService.java
+++ Account-Service/src/main/java/ie/atu/accountservice/AccountService.java
@@ -108,7 +108,7 @@ public class AccountService {
 
     public ResponseEntity<String> deleteAcc(String name) {
         Optional<Account> account = accountRepository.findByName(name);
-        if(checkBal(name) == 0) {
+        if(checkBal(name) < 1) {
             if(account.isPresent()){
                 Account accountCurrent = account.get();
                 accountRepository.delete(accountCurrent);
